b4ad75e99c20bfe873665cc0f6fcbca2aceb4eba
Oqa <oqaislenning@gmail.com>
2022-12-02 17:02:47 +0100

    solution affichage
    opui

--- Project/main.c
+++ Project/main.c
@@ -1,54 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 
 int main()
 {
+    int colonne=0;
+    int ligne=0;
+    char c;
+    char chaine[1024];
+    int actuel=0;
     FILE * fichier = fopen("../annuaire.csv","r+");
     if (!fichier){
         printf("Erreur, impossible d'ouvrir le fichier");
+        exit(1);
     }
-    else{
-        char buffer[1024];
-        int colonne = 0;
-        int ligne = 0;
 
-        while(fgets(buffer, 512, fichier)){
-            colonne=0;
+    while(c != EOF)
+    {
+        c = fgetc(fichier);
 
-            char* valeur = strtok(buffer, ", ");
-
-            while(valeur){
-                if(colonne == 0){
-                    printf("\n Nom : ");
-                }
-                if(colonne ==1){
-                    printf("\n Prénom : ");
-                    }
-                if(colonne ==2){
-                    printf("\n Code postal : ");
-                    }
-                if(colonne ==3){
-                    printf("\n Ville :");
-                    }
-                if(colonne ==4){
-                    printf("\n Numero : ");
-                    }
-                if(colonne ==5){
-                    printf("\n Email : ");
-                    }
-                if(colonne ==6){
-                    printf("\n Metier : ");
-                    }
-                printf("%s", valeur);
-                valeur = strtok(NULL, ", ");
-                colonne++;
-                ligne++;
-                }
-                printf("\n");
-
-            }
-            fclose(fichier);
+        if (c == ',')
+        {
+            colonne++;
+            printf("\t");
         }
-        return 0;
+        else if (c == '\n')
+        {
+            ligne++;
+            colonne=0;
+            actuel=0;
+            printf("\n");
+        }
+        else
+        {
+
+            chaine[actuel] = c;
+            printf("%c", chaine[actuel]);
+            actuel++;
+        }
     }
+    fclose(fichier);
+    return 0;
+}
