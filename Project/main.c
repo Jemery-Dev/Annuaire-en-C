1c8b89bcf7499563ecd7e3f8f1929899fb75f640
Oqa <oqaislenning@gmail.com>
2022-12-24 13:45:25 +0100

    aa
    a

--- Project/main.c
+++ Project/main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-    int colonne=0;
-    int ligne=0;
-    char c;
-    char chaine[1024];
-    int actuel=0;
     FILE * fichier = fopen("../annuaire.csv","r+");
     if (!fichier){
         printf("Erreur, impossible d'ouvrir le fichier");
         exit(1);
     }
-
-    while(c != EOF)
-    {
+    int colonne=0;
+    int ligne=0;
+    char c;
+    char chaine[1024];
+    int actuel=0;
+        while(c != EOF)
+        {
         c = fgetc(fichier);
 
         if (c == ',')
@@ -37,6 +37,8 @@ int main()
             printf("%c", chaine[actuel]);
             actuel++;
         }
+
+
     }
     fclose(fichier);
     return 0;
