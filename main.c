d39c0eb05913d9654452e207b536a4bdef456e08
Gheribi Abd-el-rahmen <117765552+TawahinBeirut@users.noreply.github.com>
2022-12-24 12:37:54 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define nom_fic "../../annuaire.txt"
+#include "Fonctions.H"
+int main()
+{
+    FILE*annuaire = fopen(nom_fic,"r+");
+    Remplissage_BD(annuaire);
+    int Max = Taille_BD();
+    affichage_BD(Max);
+    return 0;
+}
