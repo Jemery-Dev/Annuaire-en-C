7871b4b8b5def33d1f9702494204647e40543eca
Gheribi Abd-el-rahmen <117765552+TawahinBeirut@users.noreply.github.com>
2022-12-26 17:56:39 +0100

    Add files via upload

--- Ajouter_client.c
+++ Ajouter_client.c
@@ -5,49 +5,62 @@
 #define taille 700
 #include "Fonctions.H"
 
+
+void supprimer_entrees(char c[taille]){
+    int i;
+    for(i=0;i<=taillec;i++){
+        if (c[i]=='\n' || c[i]==' ') c[i]=NULL;
+    }
+}
+
 void ajouter_client(FILE*file){
     int nbr_client;
-    int repet;
+    int max = Taille_BD();
     printf("Entrez le nombre de client que vous voulez ajoutez \n");
     scanf("%d",&nbr_client);
     for(int i=0;nbr_client>i;i++)
     {
-        char mel[taillec];
+        int repet=0;
         printf("Entrez le mel du client :\n");
-        scanf("%s", mel);
+        scanf("%s",&BD[max+(i+1)].mel);
         // On regarde si il y a déjà le même mel pour deux clients
-        repet=0;
-        for(int i=0;i<taille;i++){
-            if (strcmp(mel,BD[i].mel)==0)
+        for(int j=0;j<max+i;j++){
+            if (strcmp(BD[max+(i+1)].mel,BD[j+i].mel)==0)
             {
                 repet=repet+1;
             }
         }
+
         if (repet==0){
-        char nom[taillec];
         printf("Entrez le nom du client :\n");
-        scanf("%s", nom);
-        char prenom[taillec];
+        fgets(BD[max+1].nom,taillec,stdin);
+        supprimer_entrees(BD[max+(i+1)].nom);
+
         printf("Entrez le prenom du client :\n");
-        scanf("%s", prenom);
-        char code_postal[taillec];
+        fgets(BD[max+1].prenom,taillec,stdin);
+        supprimer_entrees(BD[max+(i+1)].prenom);
+
         printf("Entrez le code postal du client :\n");
-        scanf("%s", code_postal);
-        char ville[taillec];
+        fgets(BD[max+1].code_postal,taillec,stdin);
+        supprimer_entrees(BD[max+(i+1)].code_postal);
+
         printf("Entrez le ville du client :\n");
-        scanf("%s", ville);
-        char telephone[taillec];
+        fgets(BD[max+1].ville,taillec,stdin);
+        supprimer_entrees(BD[max+(i+1)].ville);
+
         printf("Entrez le telephone du client :\n");
-        scanf("%s", telephone);
-        char profession[taillec];
+        fgets(BD[max+1].telephone,taillec,stdin);
+        supprimer_entrees(BD[max+(i+1)].telephone);
+
         printf("Entrez la profession du client :\n");
-        scanf("%s", profession);
-        file = fopen("annuaire.txt", "a");
-        fprintf(file,"\n%s,%s,%s,%s,%s,%s,%s",nom,prenom,code_postal,ville,telephone,mel,profession);
-        fclose(file);
+        fgets(BD[max+1].profession,taillec,stdin);
+        supprimer_entrees(BD[max+(i+1)].profession);
+
+        fseek(file,SEEK_SET,SEEK_END);
+        fprintf(file,"\n %s,%s,%s,%s,%s,%s,%s",BD[max+(i+1)].nom,BD[max+(i+1)].prenom,BD[max+(i+1)].code_postal,BD[max+(i+1)].ville,BD[max+(i+1)].telephone,BD[max+(i+1)].mel,BD[max+(i+1)].profession);
         printf("Le client a ete ajoute avec succès\n");
         }
-        if (repet>0){
+        else{
             printf("Le client n'a pas pu etre ajoute car ce mel existe deja\n");
         }
     }
