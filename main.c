cd4d40e2aac494df8f225f5a9b81e82e69241726
Oqaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <75862623+Oqaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-01-08 20:13:41 +0100

    autre commit
    a

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Fonctions.H"
-#define nom_fic "annuaire.txt"
+#define nom_fic "annuaire.csv"
 int main()
 {
     int i,j,arr=0;
@@ -21,52 +21,52 @@ int main()
     scanf("%d",&i);
         switch(i){
             case 1: printf("**Entrez les données de ce nouveau client\n");
-                    printf("*Nom?");
+                    printf("*Nom?\n");
                     getchar();
                     lire(nom,taillec);
-                    printf("*Prenom?");
+                    printf("*Prenom?\n");
                     lire(prenom,taillec);
-                    printf("*Code postal?");
+                    printf("*Code postal?\n");
                     lire(code_postal,taillec);
-                    printf("*Ville?");
+                    printf("*Ville?\n");
                     lire(ville,taillec);
-                    printf("*Telephone?");
+                    printf("*Telephone?\n");
                     lire(telephone,taillec);
-                    printf("*Mel?");
+                    printf("*Mel?\n");
                     lire(mel,taillec);
-                    printf("*Profession?");
+                    printf("*Profession?\n");
                     lire(profession,taillec);
                     ajouter_client(nom_fic,nom,prenom,code_postal,ville,telephone,mel,profession);
                     break;
-            case 2: printf("Voulez vous modifier un mail(1) ou un autre champ(2)?");
+            case 2: printf("Voulez vous modifier un mail(1) ou un autre champ(2)?\n");
                     scanf("%d",&j);
                     if(j==1){
-                        printf("*Quel mail voulez vous modifier?");
+                        printf("*Quel mail voulez vous modifier?\n");
                         getchar();
                         lire(mel,taillec);
-                        printf("*Par quoi voulez vous remplacer?");
+                        printf("*Par quoi voulez vous remplacer?\n");
                         lire(nv_mel,taillec);
                         Modifier_mel_client(nom_fic,mel,nv_mel);
                     }
                     else {
                     if(j==2){
-                    printf("*Saisissez l'addresse mail pour modifier.");
+                    printf("*Saisissez l'addresse mail pour modifier.\n");
                     getchar();
                     lire(mel,taillec);
-                    printf("*Quel champ voulez vous modifier ?");
+                    printf("*Quel champ voulez vous modifier ?\n");
                     lire(nom_champ,taillec);
-                    printf("*Que voulez vous y mettre?");
+                    printf("*Que voulez vous y mettre?\n");
                     lire(val_chaine,taillec);
                     modifier_autres_que_mel(nom_fic,mel,nom_champ,val_chaine);
                     }
-                    else printf("Veuillez choisir une option valide");}
+                    else printf("Veuillez choisir une option valide\n");}
                     break;
-            case 3: printf("*Quelle est l'adresse mail du client que vous souhaitez supprimer?");
+            case 3: printf("*Quelle est l'adresse mail du client que vous souhaitez supprimer?\n");
                     getchar();
                     lire(mel,taillec);
                     supprimer_client(nom_fic,mel);
                     break;
-            case 4: printf("Voulez vous afficher la base de données comme tel(1)ou triée par ordre alphabetique?(2)");
+            case 4: printf("Voulez vous afficher la base de donnees comme tel(1)ou trie par ordre alphabetique?(2)\n");
                     scanf("%d",&j);
                     if(j==1){
                     getchar();
@@ -78,26 +78,26 @@ int main()
                     }
                     else printf("Veuillez saisir un nombrre valide \n");}
                     break;
-            case 5: printf("*Avec combien de champs voulez vous filtrer l'annuaire");
+            case 5: printf("*Avec combien de champs voulez vous filtrer l'annuaire?\n");
                     scanf("%d",&j);
                     if (j==1){
                     getchar();
-                    printf("*Filtrer par quel champ?");
+                    printf("*Filtrer par quel champ?\n");
                     lire(nom_champ,taillec);
-                    printf("*Filtrer par quoi?");
+                    printf("*Filtrer par quoi?\n");
                     lire(val_chaine,taillec);
                     filtrer_un_champ(nom_fic,nom_champ,val_chaine);
                     }
                     else{
                     if (j==2){
                         getchar();
-                        printf("*Filtrer par quel premier champ?");
+                        printf("*Filtrer par quel premier champ?\n");
                         lire(nom_champ,taillec);
-                        printf("*Filtrer par quoi ?");
+                        printf("*Filtrer par quoi ?\n");
                         lire(val_chaine,taillec);
-                        printf("*Filtrer par quel deuxieme champ?");
+                        printf("*Filtrer par quel deuxieme champ?\n");
                         lire(nom_champ2,taillec);
-                        printf("*Filtrer par quoi ?");
+                        printf("*Filtrer par quoi ?\n");
                         lire(val_chaine2,taillec);
                         filtrer_combiner_2_champs(nom_fic,nom_champ,nom_champ2,val_chaine,val_chaine2);
                     }
@@ -107,7 +107,7 @@ int main()
             case 6: afficher_clients_donnees_manquantes(nom_fic);
                     break;
             case 7: Sauvegarder(nom_fic);
-                    printf("Fait");
+                    printf("Fait\n");
                     break;
             case 0: arr=1;
                     break;
